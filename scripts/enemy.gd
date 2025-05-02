2317e59575bbc485498f290c4c56de3d9d269405
DogWizardStudios <DogWizardStudios@gmail.com>
2025-05-02 13:12:55 +0100

    Expanded level

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,8 +2,12 @@ extends CharacterBody2D
 
 @onready var player = %Player
 @onready var animated_sprite = $AnimatedSprite2D
-@export var SPEED = 100
+@export var SPEED: float = 100.0
+@onready var enemy_speed = $"../../UI/Enemy Speed"
+@export var speed_growth = 4
 
+func _process(delta):
+	enemy_speed.text = str(SPEED).pad_decimals(2)
 
 func _physics_process(delta):
 	
@@ -13,5 +17,5 @@ func _physics_process(delta):
 	elif velocity.x < 0:
 		animated_sprite.flip_h = false
 	
-	
+	SPEED += (speed_growth * delta)
 	move_and_slide()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,31 +1,26 @@
 extends CharacterBody2D
 
 var jump_count:int = 0
-var max_jumps = 2
+var max_jumps = 1
 
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
-
+@onready var jumps = $"../UI/Jumps"
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		jump_count += 1
-	elif Input.is_action_just_pressed("jump") and jump_count < max_jumps:
-		velocity.y = 0
-		velocity.y += JUMP_VELOCITY
-		jump_count += 1
-	
-	
-	#reset jumps on returning to ground
-	if is_on_floor():
+	else:
 		jump_count = 0
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and jump_count < max_jumps:
+		jump_count += 1
+		velocity.y = 0
+		velocity.y = JUMP_VELOCITY
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -53,3 +48,6 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func _process(delta):
+	jumps.text = str(max_jumps - jump_count)
