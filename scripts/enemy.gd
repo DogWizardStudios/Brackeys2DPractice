ef3a0d9c20a9101298d09f41c445fc7bb37ae9fa
DogWizardStudios <DogWizardStudios@gmail.com>
2025-05-02 09:26:52 +0100

    Flip sprite depending on direction

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 
 @onready var player = %Player
-
+@onready var animated_sprite = $AnimatedSprite2D
 @export var SPEED = 100
 
 
 func _physics_process(delta):
 	
 	velocity = position.direction_to(player.position) * SPEED 
+	if velocity.x > 0:
+		animated_sprite.flip_h = true
+	elif velocity.x < 0:
+		animated_sprite.flip_h = false
+	
 	
 	move_and_slide()
